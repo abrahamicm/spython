a494ecdfc808d1cc6f3526525deb5645f144a7dd
abrahamicm <32270874+abrahamicm@users.noreply.github.com>
2017-10-21 22:00:08 -0400

    Create renombrar.py

---
+++ renombrar.py
@@ -0,0 +1,6 @@
+import os
+
+directorios =os.getcwd().split("\\")
+directorio = directorios[len(directorios)-1]
+a=os.listdir(".")	
+for x in os.listdir("."): os.rename(x, x.replace("Screenshot_", directorio + " "))
